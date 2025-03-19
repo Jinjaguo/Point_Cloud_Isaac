2b1bdf8bc3e915ba139f3fbf061165fbdba7e4ff
jinjaguo <jinjaguo@umich.edu>
2025-03-18 22:05:05 -0400

    feat: 实现不同学习率的手指更新函数 添加了 update_with_dif_lr 函数，并实现了不同的手指梯度缩放因子，优化了手指更新过程

--- only_sdf_planner.py
+++ only_sdf_planner.py
@@ -6,6 +6,8 @@ FINGER_TO_IDX = {
     'middle': 1,
     'thumb': 2
 }
+
+
 def _preprocess_fingers(q, contact_scenes):
     """
     :param q: (1, 1, 16)
@@ -44,7 +46,7 @@ def _preprocess_fingers(q, contact_scenes):
 
 
 def contact_constraints(q, finger_name, contact_scenes, compute_grads=True, compute_hess=False, terminal=False,
-                         projected_diffusion=False):
+                        projected_diffusion=False):
     """
 
         :param q: state tensor shape [N, T, 16],
@@ -59,7 +61,7 @@ def contact_constraints(q, finger_name, contact_scenes, compute_grads=True, comp
         g: shape [N, M], M = T-offset or 1
         grad_g: shape [N, M, T*d], or the shape you want
     """
-    N, T, _ = q.shape # [1, 1, 16]
+    N, T, _ = q.shape  # [1, 1, 16]
     data = _preprocess_fingers(q, contact_scenes)
     ret_scene = data[finger_name]
     g = ret_scene.get('sdf').reshape(N, 1, 1)
@@ -75,10 +77,10 @@ def contact_constraints(q, finger_name, contact_scenes, compute_grads=True, comp
         grad_g = torch.zeros(N, T, T, d, device=q.device)
         grad_g[:, T_range, T_range, :16] = grad_g_q[:, T_offset:]
         grad_g[:, T_range, T_range, 16: 16 + 3] = grad_g_theta.reshape(N, T + T_offset, 3)[:,
-                                                             T_offset:]
+                                                  T_offset:]
         grad_g = grad_g.reshape(N, -1, T, d)
         grad_g = grad_g.reshape(N, -1, T * d)
-        print(grad_g.shape) # torch.Size([1, 1, 47])
+        print(grad_g.shape)  # torch.Size([1, 1, 47])
         if terminal:
             grad_g = grad_g[:, -1].reshape(N, 1, T * d)
     else:
@@ -91,9 +93,10 @@ def contact_constraints(q, finger_name, contact_scenes, compute_grads=True, comp
     return g, grad_g, None
 
 
-def update(q_state, contact_scenes, finger_list=('index','middle','thumb'), max_steps=200, threshold=1e-3):
+def update(q_state, contact_scenes, finger_list=('index', 'middle', 'thumb'), max_steps=200, threshold=1e-3):
     """
-    :param q_state: shape [1, 1, 16], where the first 12 dimensions are fingers (4 fingers x 3 dof), the last 4 dimensions are objects (roll, pitch, yaw, etc).
+    :param q_state: shape [1, 1, 16], where the first 12 dimensions are fingers (4 fingers x 3 dof), the last 4
+    dimensions are objects (roll, pitch, yaw, etc).
     :param contact_scenes: the object provided by scene_collision_check(...)
     :param finger_list: the list of fingers to optimize
     :param max_steps: maximum number of optimization steps
@@ -135,7 +138,7 @@ def update(q_state, contact_scenes, finger_list=('index','middle','thumb'), max_
 
             # cost=0.5*g^2 => grad(cost)=g * grad(g)
             # this is the gradient of the cost function w.r.t. q_tensor
-            partial_grad = (gf_flat.unsqueeze(-1) * gradgf_flat).sum(dim=0) #[16]
+            partial_grad = (gf_flat.unsqueeze(-1) * gradgf_flat).sum(dim=0)  # [16]
             # print(f'finger_name:{finger_name}, partial_grad:{partial_grad}')
             partial_grad = partial_grad.view(q_tensor.shape)  # shape=[1, 1, 16]
             total_grad += partial_grad
@@ -160,13 +163,78 @@ def update(q_state, contact_scenes, finger_list=('index','middle','thumb'), max_
     return q_tensor
 
 
-def update_with_dif_lr(q_state, contact_scenes, finger_list=('index','middle','thumb'), max_steps=200, threshold=1e-3):
+def update_with_dif_lr(q_state, contact_scenes, finger_list=('index', 'middle', 'thumb'), max_steps=200,
+                       threshold=1e-3):
     """
-    :param q_state: shape [1, 1, 16], where the first 12 dimensions are fingers (4 fingers x 3 dof), the last 4 dimensions are objects (roll, pitch, yaw, etc).
+    :param q_state: shape [1, 1, 16], where the first 12 dimensions are fingers (4 fingers x 3 dof), the last 4
+    dimensions are objects (roll, pitch, yaw, etc).
     :param contact_scenes: the object provided by scene_collision_check(...)
     :param finger_list: the list of fingers to optimize
     :param max_steps: maximum number of optimization steps
     :param threshold: the threshold for early stopping
     :return: the optimized q_tensor
-    """
-    pass
+     """
+
+    q_tensor = q_state.reshape(1, 1, 16).clone().detach()
+    q_tensor.requires_grad_(True)
+    optimizer = torch.optim.Adam([q_tensor], lr=1e-3)
+
+    step = 0
+    import time
+    s = time.time()
+
+    scaling_factors = {'index': 0.8, 'middle': 3.0, 'thumb': 0.5}  # middle 手指梯度放大 2 倍
+    while step < max_steps:
+        step += 1
+        # first we zero the gradients
+        optimizer.zero_grad()
+        total_grad = torch.zeros_like(q_tensor)
+
+        data = _preprocess_fingers(q_tensor, contact_scenes)
+        sdf_vals = {}
+        # calculate sdf and grad
+        for finger_name in finger_list:
+
+            g = data[finger_name]['sdf']  # => shape [N, T], [1,1]
+            grad_sdf = data[finger_name]['grad_sdf']  # => [N, T, 16]
+
+            # print(grad_sdf)
+            import torch.nn.functional as F
+            g_eff = F.relu(g)  # g_eff = max(g, 0), a non-negative sdf value
+            sdf_vals[finger_name] = g_eff.mean().item()  # record
+            # partial_grad
+            gf_flat = g_eff.view(-1)  # shape=[1]
+            if finger_name == 'thumb':
+                #
+                gradgf_flat = -grad_sdf.view(-1, q_tensor.shape[-1])  # shape=[1, 16]
+            else:
+                gradgf_flat = grad_sdf.view(-1, q_tensor.shape[-1])  # shape=[1, 16]
+
+            # cost=0.5*g^2 => grad(cost)=g * grad(g)
+            # this is the gradient of the cost function w.r.t. q_tensor
+            partial_grad = (gf_flat.unsqueeze(-1) * gradgf_flat).sum(dim=0)  # [16]
+            partial_grad = partial_grad.view(q_tensor.shape)  # shape=[1, 1, 16]
+
+            # 这里放大 middle 手指的梯度
+            partial_grad *= scaling_factors[finger_name]
+            total_grad += partial_grad
+
+        q_tensor.grad = total_grad
+        # q_tensor has been updated
+        optimizer.step()  # Adam updates q_tensor
+
+        print(f"Step {step}:")
+        for finger in ['index', 'middle', 'thumb']:
+            print(f"  {finger}: SDF = {sdf_vals[finger]:.6f}")
+
+        if max(sdf_vals.values()) < threshold:
+            print('time for solving contact constraint:', time.time() - s)
+            # # print the sdf values of each finger
+            # print("Final SDF values after optimization:")
+            # for finger in ['index', 'middle', 'thumb']:
+            #     print(f"  {finger}: SDF = {sdf_vals[finger]:.6f}")
+            break  # if the contact constraint is satisfied, break the loop
+
+        # return the optimized q_tensor
+    return q_tensor
+
